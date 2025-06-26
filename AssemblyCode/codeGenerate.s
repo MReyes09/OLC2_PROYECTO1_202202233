cd36881fce5c361ac33a33b35c778c5151b2bd8a
MatthewReyes <matthewrm99@gmail.com>
2025-06-26 02:44:07 -0600

    falta aplicar un metodo de limpieza de registros

--- AssemblyCode/codeGenerate.s
+++ AssemblyCode/codeGenerate.s
@@ -21,34 +21,357 @@ main:
 stp x29, x30, [sp, #-16]!
 mov x29, sp
 SUB SP, SP, #72
-//  ------------- VisitAddSub -------------
-// Entero: 6
-MOV x1, #6
-STR x1, [x29, #-16]
-//  ------------- VisitAddSub -------------
-// Entero: 9
-MOV x1, #9
-STR x1, [x29, #-24]
-// Entero: 8
-MOV x1, #8
-STR x1, [x29, #-32]
-// SUMA DE ENTEROS
-LDR x2, [x29, #-24]
-LDR x1, [x29, #-32]
-ADD x0, x2, x1
-STR x0, [x29, #-40]
-// SUMA DE ENTEROS
-LDR x2, [x29, #-16]
-LDR x1, [x29, #-40]
-ADD x0, x2, x1
-STR x0, [x29, #-48]
-// Declaracion inferida: numero
-LDR x1, [x29, #-48]
+// Entero: 10
+MOV x1, #10
+// Declaracion inferida: x
 MOV x0, x1
+STR x0, [x29, #-16]
+// Función embebida: println
+// cadena: "hola x ="
+MOV x11, x10
+// Byte: 104 uso de Heap: 'h'
+MOV w0, #104
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 111 uso de Heap: 'o'
+MOV w0, #111
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 108 uso de Heap: 'l'
+MOV w0, #108
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 97 uso de Heap: 'a'
+MOV w0, #97
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 32 uso de Heap: ' '
+MOV w0, #32
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 120 uso de Heap: 'x'
+MOV w0, #120
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 32 uso de Heap: ' '
+MOV w0, #32
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 61 uso de Heap: '='
+MOV w0, #61
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 0 uso de Heap: '\x00'
+MOV w0, #0
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+MOV x0, x11
+MOV X0, x0
+BL print_cadena
+MOV X0, #1
+ADR X1, espacio_str
+MOV X2, #1
+MOV W8, #64
+SVC #0
+// Identificador: x
+LDR x1, [x29, #-16]
+// Preparando entero para imprimir
+LDR x1, [x29, #-16]
+MOV X0, x1
+BL print_entero
+MOV X0, #1
+ADR X1, espacio_str
+MOV X2, #1
+MOV W8, #64
+SVC #0
+MOV X0, #1
+ADR X1, salto_linea_str
+MOV X2, #1
+MOV W8, #64
+SVC #0
+// cadena: " papito"
+MOV x11, x10
+// Byte: 32 uso de Heap: ' '
+MOV w0, #32
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 112 uso de Heap: 'p'
+MOV w0, #112
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 97 uso de Heap: 'a'
+MOV w0, #97
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 112 uso de Heap: 'p'
+MOV w0, #112
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 105 uso de Heap: 'i'
+MOV w0, #105
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 116 uso de Heap: 't'
+MOV w0, #116
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 111 uso de Heap: 'o'
+MOV w0, #111
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 0 uso de Heap: '\x00'
+MOV w0, #0
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Declaracion inferida: y
+MOV x0, x11
+STR x0, [x29, #-24]
+// cadena: "valio madres"
+MOV x11, x10
+// Byte: 118 uso de Heap: 'v'
+MOV w0, #118
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 97 uso de Heap: 'a'
+MOV w0, #97
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 108 uso de Heap: 'l'
+MOV w0, #108
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 105 uso de Heap: 'i'
+MOV w0, #105
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 111 uso de Heap: 'o'
+MOV w0, #111
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 32 uso de Heap: ' '
+MOV w0, #32
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 109 uso de Heap: 'm'
+MOV w0, #109
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 97 uso de Heap: 'a'
+MOV w0, #97
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 100 uso de Heap: 'd'
+MOV w0, #100
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 114 uso de Heap: 'r'
+MOV w0, #114
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 101 uso de Heap: 'e'
+MOV w0, #101
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 115 uso de Heap: 's'
+MOV w0, #115
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Byte: 0 uso de Heap: '\x00'
+MOV w0, #0
+STRB w0, [x10]
+MOV x0, #1
+ADD x10, x10, x0
+// Declaracion inferida: z
+MOV x0, x11
+STR x0, [x29, #-32]
+// Función embebida: println
+//  ------------- VisitAddSub -------------
+// Identificador: z con profundidad: 0
+LDR x0, [x29, #-32]
+STR x0, [x29, #-40]
+// Identificador: y con profundidad: 0
+LDR x0, [x29, #-24]
+STR x0, [x29, #-48]
+// CONCATENACION DE CADENAS
+LDR x0, [x29, #-40]
+LDR x1, [x29, #-48]
+ADR x10, buffer
+BL concatenar_cadena
 STR x0, [x29, #-56]
+// Preparando string para imprimir
+LDR x11, [x29, #-56]
+MOV x0, x11
+MOV X0, x0
+BL print_cadena
+MOV X0, #1
+ADR X1, espacio_str
+MOV X2, #1
+MOV W8, #64
+SVC #0
+MOV X0, #1
+ADR X1, salto_linea_str
+MOV X2, #1
+MOV W8, #64
+SVC #0
 L1:
 ADD sp, sp, #72
 LDP x29, x30, [sp], #16
 RET
 
 //Funciones De Impresion:
+
+		.align 2
+		print_cadena:
+			stp     x29, x30, [sp, #-16]!
+			stp     x19, x20, [sp, #-16]!
+			mov     x19, x0
+		.align 2
+		print_loop:
+			ldrb    w20, [x19]
+			cbz     w20, print_done
+			mov     x0, #1      
+			mov     x1, x19
+			mov     x2, #1
+			mov     x8, #64             
+			svc     #0      
+			add     x19, x19, #1
+			b       print_loop
+		.align 2    
+		print_done:
+			ldp     x19, x20, [sp], #16
+			ldp     x29, x30, [sp], #16
+			ret
+	
+
+		.align 2
+		print_entero:
+			stp x29, x30, [sp, #-16]!
+			stp x19, x20, [sp, #-16]!
+			stp x21, x22, [sp, #-16]!
+			stp x23, x24, [sp, #-16]!
+			stp x25, x26, [sp, #-16]!
+			stp x27, x28, [sp, #-16]!
+			mov x19, x0
+			cmp x19, #0 
+			bge numero_positivo   
+			mov x0, #1                 
+			adr x1, signo_menos1        
+			mov x2, #1                 
+			mov w8, #64                
+			svc #0
+			neg x19, x19               
+		.align 2
+		numero_positivo:
+			sub sp, sp, #32            
+			mov x22, sp                
+			mov x23, #0            
+			cmp x19, #0
+			bne bucle_conversion
+			mov w24, #48           
+			strb w24, [x22, x23]
+			add x23, x23, #1 
+			b print_resultado
+		.align 2
+		bucle_conversion:
+			mov x24, #10
+			udiv x25, x19, x24
+			msub x26, x25, x24, x19
+			add x26, x26, #48         
+			strb w26, [x22, x23]     
+			add x23, x23, #1          
+			mov x19, x25               
+			cbnz x19, bucle_conversion 
+			mov x27, #0      
+		.align 2
+		bucle_inversion:
+			sub x28, x23, x27        
+			sub x28, x28, #1     
+			cmp x27, x28              
+			bge print_resultado  
+			ldrb w24, [x22, x27]      
+			ldrb w25, [x22, x28]       
+			strb w25, [x22, x27]       
+			strb w24, [x22, x28]      
+			add x27, x27, #1           
+			b bucle_inversion         
+		.align 2
+		print_resultado:
+			mov x0, #1                 
+			mov x1, x22               
+			mov x2, x23               
+			mov w8, #64              
+			svc #0
+			// Restaurar registros
+			add sp, sp, #32            
+			ldp x27, x28, [sp], #16   
+			ldp x25, x26, [sp], #16
+			ldp x23, x24, [sp], #16
+			ldp x21, x22, [sp], #16
+			ldp x19, x20, [sp], #16
+			ldp x29, x30, [sp], #16    
+			ret                       
+		.align 2
+		signo_menos1:
+			.ascii "-"
+	
+
+        .align 2
+        concatenar_cadena:
+            stp x29, x30, [sp, #-16]!
+            stp x19, x20, [sp, #-16]!
+            stp x21, x22, [sp, #-16]!
+            mov x19, x0
+            mov x20, x1 
+            mov x21, x10 
+        copiar_primera:
+            ldrb w22, [x19]
+            cbz w22, primera_terminada
+            strb w22, [x10]
+            add x19, x19, #1
+            add x10, x10, #1
+            b copiar_primera
+        primera_terminada:
+        copiar_segunda:
+            ldrb w22, [x20]
+            strb w22, [x10]
+            cbz w22, concatenacion_completa
+            add x20, x20, #1
+            add x10, x10, #1
+            b copiar_segunda
+        concatenacion_completa:
+            add x10, x10, #1
+            mov x0, x21
+            ldp x21, x22, [sp], #16
+            ldp x19, x20, [sp], #16
+            ldp x29, x30, [sp], #16
+            ret
+        
